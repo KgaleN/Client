686f7a5adb744ad55b50083d8c016694b3114b53
nguba <ngubaentando18@gmail.com>
2023-03-27 11:29:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static HttpClient client = new HttpClient();
+         
+        static void Main(string[] args)
+        { 
+            
+        
+          getRandomWord();
+        }
+public static void getRandomWord()
+{
+    requestWord("random");
+}
+
+public static void getSortedWords()
+{
+    requestWord("sorted");
+}
+
+public static void getAllWords()
+{
+    requestWord("all");
+}
+
+public static void requestWord(string route)
+{
+      var client = new RestClient("http://localhost:5037/controller/");
+      var request = new RestRequest(route);
+      var response = client.Execute(request);
+      var word = response.Content;
+      Console.WriteLine(word);
+}
+
+        
+       
+    }
+}
